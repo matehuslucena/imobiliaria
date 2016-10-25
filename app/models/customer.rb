0a71c91dea1728caa5185a48092eb31021cb56de
Matheus Lucena <matheuslucena@gmail.com>
2016-10-25 11:06:21 -0200

    added model associations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ActiveRecord::Base
+  has_many :houses
+
+  validates :first_name, :last_name, :phone, :document, :address, :presence => true
 
   def full_name
     self.first_name + ' ' + self.last_name
