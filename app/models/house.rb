870f50ab6bd9edb37f706a9da21f960f51a9c653
Matheus Lucena <matheuslucena@gmail.com>
2016-10-25 11:07:15 -0200

    added association with operation

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,5 +1,9 @@
 class House < ActiveRecord::Base
   belongs_to :customer
+  belongs_to :operation
+
+  validates :address, :city, :state, :zip_code, :price, :operation, :description, :presence => true
 
   delegate :full_name, to: :customer, prefix: true
+  delegate :name, to: :operation, prefix: true
 end
