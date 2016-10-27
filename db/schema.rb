70004687deb768dc734d8593aafc7d3fea407c48
Matheus Lucena <matheuslucena@gmail.com>
2016-10-27 14:55:30 -0200

    added table reservations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024223459) do
+ActiveRecord::Schema.define(version: 20161026165314) do
 
   create_table "houses", force: :cascade do |t|
     t.string   "address",      limit: 255
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20161024223459) do
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "house_id",   limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   create_table "users", force: :cascade do |t|
