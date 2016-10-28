0620a4d9dc7b1f8723da431df152713f5582ea46
Matheus Lucena <matheuslucena@gmail.com>
2016-10-27 22:49:14 -0200

    changed ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,10 +8,11 @@ class Ability
 
     elsif user.agent?
       can :manage, House
+      cannot [:destroy, :update], House
       can :manage, Reservation
 
     elsif user.customer?
-      can [:update, :destroy, :create], House, user_id: user.id
+      can [:update, :destroy, :create, :show], House, user_id: user.id
       can :read, House
       can :customer_houses, House, user_id: user.id
       can :manage, Reservation, user_id: user.id
