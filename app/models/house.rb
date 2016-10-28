59ad2f6d97c7990af9ebd05a00333a7e91b6e33c
Matheus Lucena <matheuslucena@gmail.com>
2016-10-28 11:14:33 -0200

    changes to new ruby style

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,7 +2,7 @@ class House < ActiveRecord::Base
   belongs_to :user
   belongs_to :operation
 
-  validates :address, :city, :state, :zip_code, :price, :operation, :description, :user, :presence => true
+  validates :address, :city, :state, :zip_code, :price, :operation, :description, :user, presence: true
 
   delegate :name, to: :operation, prefix: true
   delegate :name, to: :user, prefix: true
