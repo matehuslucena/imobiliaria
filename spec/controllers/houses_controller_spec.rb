baf373353fac2c14dd853cfd8c326cf10f442fc6
Matheus Lucena <matheuslucena@gmail.com>
2016-10-27 22:47:10 -0200

    refactor tests after the controller refactor

--- spec/controllers/houses_controller_spec.rb
+++ spec/controllers/houses_controller_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe HousesController, :type => :controller do
 
   shared_context 'is logged in' do
-    before(:each) do
-      sign_in create :user
+    before do
+      sign_in  create :user, role: role
     end
   end
 
@@ -12,28 +12,84 @@ describe HousesController, :type => :controller do
     it { is_expected.to redirect_to new_user_session_path }
   end
 
+  shared_examples 'assigns the houses of all houses to @houses' do
+    it { expect{ subject }.to change{ assigns(:houses) }.to([house, another_house]) }
+  end
+
+  shared_examples 'call action show with success' do
+    it { is_expected.to be_success }
+
+    it 'assigns the requested house to @house' do
+      expect{ subject }.to change{ assigns(:house) }.to(house)
+    end
+
+    it 'render the correct view' do
+      expect(subject).to render_template :show
+    end
+  end
+
+  shared_examples 'call action new with success' do
+    it 'renders the #new template' do
+      expect(subject).to render_template(:new)
+    end
+  end
+
+  shared_examples 'call action creation as admin or agent' do
+    let!(:operation) { create :operation }
+    let(:customer) { create :user, email: 't@t.com', role: :customer }
+
+    it 'creates a new house without customer_id' do
+      expect {
+        post :create, house: attributes_for(:house).merge({operation_id: operation.id })
+      }.to change(House, :count).by(0)
+    end
+
+    it 'creates a new house with customer_id' do
+
+      expect {
+        post :create, house: attributes_for(:house).merge({operation_id: operation.id, user_id: customer })
+      }.to change(House, :count).by(1)
+    end
+  end
+
   describe 'GET #index' do
-    let(:house) { create :house }
-
-    context 'when user is logged in' do
-      subject { get :index }
-
+    context 'when authenticated' do
       include_context 'is logged in'
 
-      context 'when param is valid' do
+      subject { get :index }
+
+      let!(:house) { create :house, user: controller.current_user }
+      let(:another_user) { create :user, email: 't@t.com' }
+      let(:another_house) { create :house, user: another_user }
+
+      context 'as customer' do
+        let(:role){ :customer }
+
         it { is_expected.to be_success }
 
-        it 'assigns the requested house to @house' do
-          expect{ subject }.to change{ assigns(:houses) }.to([house])
+        it 'assigns the houses of other customers to @houses' do
+          expect{ subject }.to change{ assigns(:houses) }.to([another_house])
         end
 
         it 'renders the index template' do
           expect(subject).to render_template(:index)
         end
       end
+
+      context 'as admin' do
+        let(:role){ :admin }
+
+        it_behaves_like 'assigns the houses of all houses to @houses'
+      end
+
+      context 'as agent' do
+        let(:role){ :agent }
+
+        it_behaves_like 'assigns the houses of all houses to @houses'
+      end
     end
 
-    context 'when user is not logged' do
+    context 'when not authenticated' do
       subject { get :index }
 
       it_behaves_like 'not logged in'
@@ -41,24 +97,35 @@ describe HousesController, :type => :controller do
   end
 
   describe 'GET #show' do
-    let(:house) { create :house }
+    let(:house) { create :house, user: controller.current_user }
+    let(:another_user) { create :user, email: 't@t.com' }
+    let(:another_house) { create :house, user: another_user}
+
     subject { get :show, id: house.id }
 
-    context 'when user is logged in' do
+    context 'when authenticated' do
       include_context 'is logged in'
 
-      it { is_expected.to be_success }
+      context 'as customer' do
+        let(:role){ :customer }
 
-      it 'assigns the requested house to @house' do
-        expect{ subject }.to change{ assigns(:house) }.to(house)
+        it_behaves_like 'call action show with success'
       end
 
-      it 'render the correct view' do
-        expect(subject).to render_template :show
+      context 'as admin' do
+        let(:role){ :admin }
+
+        it_behaves_like 'call action show with success'
+      end
+
+      context 'as agent' do
+        let(:role){ :agent }
+
+        it_behaves_like 'call action show with success'
       end
     end
 
-    context 'when user is not logged' do
+    context 'when not authenticated' do
       subject { get :show, id: 1 }
 
       it_behaves_like 'not logged in'
@@ -66,59 +133,86 @@ describe HousesController, :type => :controller do
   end
 
   describe 'GET #new' do
-    let(:house) { create :house }
-    subject { get :new }
-
-    context 'when user ig logged in' do
+    context 'when authenticated' do
       include_context 'is logged in'
 
-      it 'renders the #new template' do
-        expect(subject).to render_template(:new)
+      subject { get :new }
+
+      context 'as customer' do
+        let(:role){ :customer }
+
+        it_behaves_like 'call action new with success'
+      end
+
+      context 'as admin' do
+        let(:role){ :admin }
+
+        it_behaves_like 'call action new with success'
+      end
+
+      context 'as agent' do
+        let(:role){ :agent }
+
+        it_behaves_like 'call action new with success'
       end
     end
 
-    context 'when user is not logged' do
-      subject { get :new }
+    context 'when not authenticated' do
+      subject { get :show, id: 1 }
 
       it_behaves_like 'not logged in'
     end
   end
 
   describe 'POST #create' do
-    context 'when user is logged in' do
-
+    context 'when authenticated' do
       include_context 'is logged in'
 
-      context 'with valid attributes' do
+      context 'as customer' do
+        let(:role) { :customer }
+        let!(:operation) { create :operation }
+
         it 'creates a new house' do
           expect {
-            post :create, house: attributes_for(:house)
+            post :create, house: attributes_for(:house).merge({operation_id: operation.id })
           }.to change(House, :count).by(1)
         end
 
         it 'redirects to the new house' do
-          post :create, house: attributes_for(:house)
+          post :create, house: attributes_for(:house).merge({operation_id: operation.id })
 
           expect(response).to redirect_to house_path(House.last)
         end
+
+        context 'with invalid attributes' do
+          it 'does not save the new house' do
+            expect {
+              post :create, house: attributes_for(:invalid_house)
+            }.to_not change(House, :count)
+          end
+
+          it 're-renders the new method' do
+            post :create, house: attributes_for(:invalid_house)
+
+            expect(response).to render_template :new
+          end
+        end
       end
 
-      context 'with invalid attributes' do
-        it 'does not save the new house' do
-          expect {
-            post :create, house: attributes_for(:invalid_house)
-          }.to_not change(House, :count)
-        end
+      context 'as admin' do
+        let(:role) { :admin }
 
-        it 're-renders the new method' do
-          post :create, house: attributes_for(:invalid_house)
+        it_behaves_like 'call action creation as admin or agent'
+      end
 
-          expect(response).to render_template :new
-        end
+      context 'as agent' do
+        let(:role) { :agent }
+
+        it_behaves_like 'call action creation as admin or agent'
       end
     end
 
-    context 'when user is not logged' do
+    context 'when not authenticated' do
       subject { post :create, house: attributes_for(:invalid_house) }
 
       it_behaves_like 'not logged in'
@@ -126,54 +220,83 @@ describe HousesController, :type => :controller do
   end
 
   describe 'PUT #update' do
-    context 'when user is logged in' do
-      let(:house){ create :house }
-
+    context 'when authenticated' do
       include_context 'is logged in'
 
-      context 'valid attributes' do
+      context 'as customer' do
+        let(:role) { :customer }
+        let(:house){ create :house, user: controller.current_user }
 
-        it 'located the requested @house' do
-          put :update, id: house, house: attributes_for(:house)
+        context 'valid attributes' do
+          it 'located the requested @house' do
+            put :update, id: house, house: attributes_for(:house)
 
-          expect(assigns(:house)).to eq(house)
+            expect(assigns(:house)).to eq(house)
+          end
+
+          it 'changes @house attributes' do
+            put :update, id: house, house: attributes_for(:house, address: 'New Address')
+            house.reload
+
+            expect(house.address).to eq('New Address')
+          end
+
+          it 'redirects to the updated house' do
+            put :update, id: house, house: attributes_for(:house)
+
+            expect(response).to redirect_to house_path(house)
+          end
         end
 
-        it 'changes @house attributes' do
-          put :update, id: house, house: attributes_for(:house, address: 'New Address')
-          house.reload
+        context 'invalid attributes' do
+          it 'locates the requested @house' do
+            put :update, id: house, house: attributes_for(:invalid_house)
+            expect(assigns(:house)).to eq(house)
+          end
 
-          expect(house.address).to eq('New Address')
-        end
+          it 'does not change @house attributes' do
+            put :update, id: house, house: attributes_for(:house, address: nil, city: 'New City')
+            house.reload
 
-        it 'redirects to the updated house' do
-          put :update, id: house, house: attributes_for(:house)
+            expect(house.city).not_to eq('New City')
+          end
 
-          expect(response).to redirect_to house_path(house)
+          it 're-renders the edit method' do
+            put :update, id: house, house: attributes_for(:invalid_house)
+            expect(response).to render_template :edit
+          end
         end
       end
 
-      context 'invalid attributes' do
-        it 'locates the requested @house' do
-          put :update, id: house, house: attributes_for(:invalid_house)
-          expect(assigns(:house)).to eq(house)
-        end
+      context 'as agent' do
+        let(:role) { :agent }
+        let(:house){ create :house, user: controller.current_user }
 
-        it 'does not change @house attributes' do
-          put :update, id: house, house: attributes_for(:house, address: nil, city: 'New City')
-          house.reload
+        context 'valid attributes' do
+          it 'located the requested @house' do
+            put :update, id: house, house: attributes_for(:house)
 
-          expect(house.city).not_to eq('New City')
-        end
+            expect(assigns(:house)).to eq(house)
+          end
 
-        it 're-renders the edit method' do
-          put :update, id: house, house: attributes_for(:invalid_house)
-          expect(response).to render_template :edit
+          it 'changes @house attributes' do
+            old_address = house.address
+            put :update, id: house, house: attributes_for(:house, address: 'New Address')
+            house.reload
+
+            expect(house.address).to eq(old_address)
+          end
+
+          it 'redirects to the updated house' do
+            put :update, id: house, house: attributes_for(:house)
+
+            expect(response).to redirect_to root_path
+          end
         end
       end
     end
 
-    context 'when user is not logged' do
+    context 'when not authenticated' do
       subject { post :create, house: attributes_for(:invalid_house) }
 
       it_behaves_like 'not logged in'
@@ -181,24 +304,42 @@ describe HousesController, :type => :controller do
   end
 
   describe 'DELETE #destroy' do
-    context 'when user is logged in' do
-      let!(:house){ create :house }
-
+    context 'when authenticated' do
       include_context 'is logged in'
+      let!(:house){ create :house, user: controller.current_user }
 
-      it 'deletes the house' do
-        expect{
+      context 'as customer' do
+        let(:role) { :customer }
+
+        it 'deletes the house' do
+          expect{
+            delete :destroy, id: house
+          }.to change(House,:count).by(-1)
+        end
+
+        it 'redirects to houses#index' do
           delete :destroy, id: house
-        }.to change(House,:count).by(-1)
+          expect(response).to redirect_to houses_path
+        end
       end
 
-      it 'redirects to houses#index' do
-        delete :destroy, id: house
-        expect(response).to redirect_to houses_path
+      context 'as agent' do
+        let(:role) { :agent }
+
+        it 'deletes the house' do
+          expect{
+            delete :destroy, id: house
+          }.to change(House,:count).by(0)
+        end
+
+        it 'redirects to houses#index' do
+          delete :destroy, id: house
+          expect(response).to redirect_to root_path
+        end
       end
     end
 
-    context 'when user is not logged' do
+    context 'when not authenticated' do
       subject { post :create, house: attributes_for(:invalid_house) }
 
       it_behaves_like 'not logged in'
